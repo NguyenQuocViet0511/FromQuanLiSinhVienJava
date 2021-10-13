2ea58c39c7c97247983a54816b3f6a9fefc26c9c
nguyen viet <lonconbaby2210@gmail.com>
2021-10-13 20:01:49 +0700

    first commit

---
+++ ProjectJava/src/test/java/NewClass.java
@@ -0,0 +1,40 @@
+
+import Form_Login.Const;
+import Hiberneat.Public.Lop;
+import Hiberneat.SinhVien.SinhVien;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.swing.table.DefaultTableModel;
+import org.hibernate.query.Query;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author admin
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+    
+    
+        Scanner sc = new Scanner(System.in);
+             String  a = sc.nextLine();
+          
+           
+            if(Double.parseDouble(a) > 10)
+            {
+                System.out.println("co");
+            }
+    }
+    
+       
+}
+    
+
